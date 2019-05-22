d7f8da424761cac9ecfee80d4ea93179be77d9ce
BStrawser-TSM <50869856+BStrawser-TSM@users.noreply.github.com>
2019-05-22 12:03:38 -0400

    Product Grouping Adding

--- MyShop2/MyShop2.DataAccess.InMemory/ProductRepository.cs
+++ MyShop2/MyShop2.DataAccess.InMemory/ProductRepository.cs
@@ -11,7 +11,7 @@ namespace MyShop2.DataAccess.InMemory
     public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
-        List<Product> products = new List<Product>();
+        List<Product> products;
 
         public ProductRepository()
         {
